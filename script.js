bf4190757ad8702990d7b97bc26b2e9f8cccb222
ItzAsyran <asynasyran@gmail.com>
2023-03-02 16:00:17 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculate() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = num1 + num2;
+    document.getElementById("result").value = result;
+  }
+  
